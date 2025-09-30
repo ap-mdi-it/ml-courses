6f11ec7367a0257951b51ef8f0ed0d9cb29ca2ee
Kevin Diependaele <kevin.diependaele@ap.be>
2025-09-30 08:44:17 +0000

    dtreeviz plot update

--- src/ml_courses/utils.py
+++ src/ml_courses/utils.py
@@ -1,7 +1,11 @@
 """Utility functions."""
 
+import io
+
+import cairosvg
 import dtreeviz
-from IPython.display import SVG, display
+import matplotlib.pyplot as plt
+from PIL import Image
 
 
 def display_dtreeviz(render_obj: dtreeviz.utils.DTreeVizRender) -> None:
@@ -10,5 +14,16 @@ def display_dtreeviz(render_obj: dtreeviz.utils.DTreeVizRender) -> None:
     Args:
         render_obj (dtreeviz.utils.DTreeVizRender): The dtreeviz render object to display.
     """
-    svg = SVG(render_obj.svg())
-    display(svg)
+    png_data = cairosvg.svg2png(render_obj.svg().encode(), dpi=150)
+    # Open the PNG data with PIL
+    img: Image.Image = Image.open(io.BytesIO(png_data))
+
+    # Resize the image to make it larger
+    width, height = img.size
+    img = img.resize((int(width * 1.5), int(height * 1.5)), Image.Resampling.LANCZOS)
+
+    # Display with matplotlib - set figure size for larger display
+    plt.figure(figsize=(20, 12))
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
